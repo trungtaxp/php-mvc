7101e0ffc592e148b6166568e3c75c3ed13b5077
trungtaxp <phamtrungta2001@gmail.com>
2021-12-23 14:57:26 +0700

    config router str_replace

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 use app\core\Application;
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 
